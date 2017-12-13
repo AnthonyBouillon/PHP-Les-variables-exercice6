2f9cc7fa31b102b949a1617ebe1c76df47b781c7
Anthony <anthony_bouillon@outlook.fr>
2017-12-13 08:47:46 +0100

    Les variables

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
-<?php
-$name = 'Bernard';
-echo 'Bonjour'. ' ' .$name. ' ' . ', comment vas tu ?';
- ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <?php
+    $name = 'Bernard';
+    echo 'Bonjour'. ' ' .$name. ' ' . ', comment vas tu ?';
+    ?>
+  </body>
+</html>
